ebc0d8e70dbf87f5f52b5e075a87b200f450f595
afreuljoy <afreuljoy@gmail.com>
2023-05-01 15:55:14 +0600

    get,post,put,delete http method added

--- app.js
+++ app.js
@@ -4,5 +4,19 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Server returned");
 });
+//-------- only get request can test on browser------
+app.get("/about", (req, res) => {
+  res.send("I am about route");
+});
+//-------- Others  api request testing need to "POSTMAN OR THUNDER CLIENT"----
+app.post("/", (req, res) => {
+    res.send("POST request succeeded");
+  });
+app.put("/", (req, res) => {
+    res.send("Put request succeeded");
+  });
+app.delete("/", (req, res) => {
+    res.send("Delete request succeeded");
+  });
 
-module.exports = app;
+module.exports = app; 
