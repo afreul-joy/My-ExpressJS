5c61d0f124cc2770d2cd788af29f1870e650fe24
afreuljoy <afreuljoy@gmail.com>
2023-05-01 23:43:03 +0600

    response json & html

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,11 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/login", (req, res) => {
-    res.send("I am login route");
-  });
+  res.send("I am login route");
+});
+
+//---------------Response HTML FILES--------------------------------
 router.get("/register", (req, res) => {
-    res.send("I am register route");
-  });
+  res.send("i am register route");
+});
+//---------------Response JSON FILES--------------------------------
+router.get("/users", (req, res) => {
+  const users = [
+    { id: 1, name: "John" },
+    { id: 2, name: "Jane" },
+    { id: 3, name: "Bob" },
+  ];
+  res.json(users);
+});
 
-
-  module.exports = router; 
+module.exports = router;
